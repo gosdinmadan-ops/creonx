47680e8301d6d7807e8ba2422e8c7b697c617d3e
gosdinmadan-ops <gosdinmadan@gmail.com>
2025-12-01 19:22:21 +0600

    Update Creon.lua

--- Creon.lua
+++ Creon.lua
@@ -29,16 +29,6 @@ if not mainSuccess then
     MainModule = {}
 end
 
-local SkySquidModule
-local skySuccess, skyErr = pcall(function()
-    SkySquidModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/gosdinmadan-ops/creonx/main/sky.lua"))()
-end)
-
-if not skySuccess then
-    warn("Не удалось загрузить sky.lua: " .. tostring(skyErr))
-    SkySquidModule = {}
-end
-
 local KillauraModule
 local killSuccess, killErr = pcall(function()
     KillauraModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/gosdinmadan-ops/creonx/main/killaura.lua"))()
@@ -1065,3 +1055,4 @@ ScreenGui.AncestryChanged:Connect(function()
 end)
 
 print("Creon X v2.1 загружен...")
+
