51c8e0628c8f833eabd7bd93659b1b69f42b8b3d
gosdinmadan-ops <gosdinmadan@gmail.com>
2025-12-25 15:47:32 +0600

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -699,7 +699,7 @@ function MainModule.ToggleFreeDash(enabled)
             local fasterSprint = boosts:FindFirstChild("Faster Sprint")
             if fasterSprint then
                 MainModule.FreeDash.OriginalSprintValue = fasterSprint.Value
-                fasterSprint.Value = 8 -- Изменено с 5 на 10
+                fasterSprint.Value = 9 -- Изменено с 5 на 10
             end
         end
         
@@ -3955,5 +3955,6 @@ LocalPlayer:GetPropertyChangedSignal("Parent"):Connect(function()
 end)
 
 return MainModule
+
 
 
